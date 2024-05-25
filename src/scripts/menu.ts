da960b38f45fa0cc43a7da3852b6b4295fa8c30d
toma09to <yamto0916@gmail.com>
2024-05-26 03:22:59 +0900

    🚑️ Edit menu.ts
    nonnullを強制する!が必要だった

--- src/scripts/menu.ts
+++ src/scripts/menu.ts
@@ -1,5 +1,5 @@
-document.querySelector('.hamburger').addEventListener('click', () => {
-  document.querySelectorAll('.nav-links a').forEach((value, key, parent) => {
+document.querySelector('.hamburger')!.addEventListener('click', () => {
+  document.querySelectorAll('.nav-links a').forEach((value, _key, _parent) => {
     value.classList.toggle('expanded');
   });
 });
