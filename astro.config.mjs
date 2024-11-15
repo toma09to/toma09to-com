d8e9d24226b658d06cd912196730453a3b34942c
toma09to <toma09to@mail.toma09to.com>
2024-11-15 15:46:51 +0900

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@ import { defineConfig } from 'astro/config';
 
 import node from "@astrojs/node";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+
   adapter: node({
     mode: "standalone"
-  })
+  }),
+
+  site: 'https://toma09to.com',
+  integrations: [sitemap()]
 });
