66026426279624a142166f20ea9f9d70793a71eb
toma09to <contact@mail.toma09to.com>
2025-01-20 04:44:21 +0900

    Reinstall astro

--- README.md
+++ README.md
@@ -2,20 +2,3 @@
 
 toma09to.comのサイトです。
 スタンドアローンで動作するのでどんなサーバーでも動作します。
-
-## 使用方法
-
-- イメージのビルド
-```sh
-docker build -t toma09to-com .
-```
-
-- コンテナの実行
-```sh
-docker run --rm -d -p 4321:4321 --name toma09to-com toma09to-com
-```
-
-- コンテナの終了
-```sh
-docker stop toma09to-com
-```
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from "@astrojs/node";
-
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
-export default defineConfig({
-  output: "server",
-
-  adapter: node({
-    mode: "standalone"
-  }),
-
-  site: 'https://toma09to.com',
-  integrations: [sitemap()]
-});
+export default defineConfig({});
