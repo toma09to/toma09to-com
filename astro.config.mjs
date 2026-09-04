3a6f0ad503c8dda9c94653ffdf1f3de9351e38ab
toma09to <toma09to@mail.toma09to.com>
2026-09-05 02:45:45 +0900

    Add detail of about\?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://toma09to.com'
+  site: 'https://toma09to.com',
+  redirects: {
+    "/about/detail": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+  }
 });
